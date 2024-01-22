8efeee3252c2b7879d84a56cd4d7941f56aba6c4
Magnus Küderli <magnusku@gmx.de>
2024-01-22 19:33:31 +0100

    added player entity

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ struct CameraMarker {
     move_speed: f32
 }
 
+#[derive(Default, Component)]
+struct Player;
+
 fn main() {
     App::new()
         .add_plugins(
@@ -22,8 +25,9 @@ fn main() {
 
         .insert_resource(LevelSelection::Index(0))
 
-        .register_ldtk_entity::<MyBundle>("MyEntityIdentifier")
-        .register_ldtk_entity::<MyBundle>("TestEntity")
+        .register_ldtk_entity::<EnemyBundle>("MyEntityIdentifier")
+        .register_ldtk_entity::<EnemyBundle>("TestEntity")
+        .register_ldtk_entity::<PlayerBundle>("Player")
         .run();
 }
 
@@ -53,6 +57,9 @@ fn change_levels(
     if inputs.just_pressed(KeyCode::R) {
         commands.insert_resource(LevelSelection::Index(1));
     }
+    else if inputs.just_pressed(KeyCode::T) {
+        commands.insert_resource(LevelSelection::Index(0));
+    }
 }
 
 fn move_camera(
@@ -66,17 +73,17 @@ fn move_camera(
     )   = camera.get_single_mut().unwrap();
 
     if inputs.pressed(KeyCode::A) {
-        transform.translation.x -= marker.move_speed;
+        transform.translation.x -= marker.move_speed * marker.zoom_speed;
     }
     else if inputs.pressed(KeyCode::D) {
-        transform.translation.x += marker.move_speed;
+        transform.translation.x += marker.move_speed * marker.zoom_speed;
     }
     
     if inputs.pressed(KeyCode::S) {
-        transform.translation.y -= marker.move_speed;
+        transform.translation.y -= marker.move_speed * marker.zoom_speed; 
     }
     else if inputs.pressed(KeyCode::W) {
-        transform.translation.y += marker.move_speed;
+        transform.translation.y += marker.move_speed * marker.zoom_speed;
     }
     
     if inputs.pressed(KeyCode::Q) {
@@ -87,16 +94,16 @@ fn move_camera(
     }
 }
 
-#[derive(Default, Component)]
-struct ComponentA;
-
-#[derive(Default, Component)]
-struct ComponentB;
 
 #[derive(Default, Bundle, LdtkEntity)]
-pub struct MyBundle {
-    a: ComponentA,
-    b: ComponentB,
+pub struct EnemyBundle {
+    #[sprite_sheet_bundle]
+    sprite_bundle: SpriteSheetBundle,
+}
+
+#[derive(Default, Bundle, LdtkEntity)]
+pub struct PlayerBundle {
+    marker: Player,
     #[sprite_sheet_bundle]
     sprite_bundle: SpriteSheetBundle,
 }
